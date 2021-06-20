33b76169d23935b2b7ec26f5810a680f112d286e
Nhan <nhanbui37@gmail.com>
2021-06-20 23:30:43 +0700

    add the editstudent

--- index.php
+++ index.php
@@ -27,8 +27,16 @@
   
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="controllers/addstudent.php">Tiếp nhận học sinh <span class="sr-only">(current)</span></a>
+      <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Học Sinh
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="controllers/addstudent.php">Tiếp nhận học sinh</a>
+            <a class="dropdown-item" href="controllers/editstudent.php">Sửa Thông tin học sinh</a>
+            <!-- <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a> -->
+          </div>
         </li>
         <li class="nav-item ">
           <a class="nav-link" href="controllers/showclass.php">Danh sách lớp</a>
